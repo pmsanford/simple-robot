3e24c24c81153be43a6cd8a817b28b88237de407
Paul Sanford <me@paulsanford.net>
2016-05-19 15:24:15 -0400

    Add registers and current instruction highlighting

--- roverstatus.js
+++ roverstatus.js
@@ -8,12 +8,24 @@ RoverStatus.prototype.draw = function(display) {
 	display.drawText(this.x + 1, this.y + 1, "Heading: " + this.rover.heading);
 	display.drawText(this.x + 1, this.y + 2, "Speed: " + this.rover.speed);
 	display.drawText(this.x + 1, this.y + 3, "Turning: " + this.rover.turn);
+	display.drawText(this.x + 1, this.y + 4, "PC: " + this.rover.pc.toString(16) +
+	                  " A: " + this.rover.regA + " CC: " +
+	                  this.rover.cc);
 	for (var y = 0x00; y <= 0xF0; y += 0x10) {
   	for (var x = 0x00; x <= 0x0F; x++) {
-  	  console.log(x + ', ' + y);
   	  var ostring = "00" + this.rover.mem[x + y].toString(16);
+  	  var color = '#ddd';
+  	  var oldcolor = display.getOptions().fg;
+  	  if ((x+y) == this.rover.pc) {
+  	    color = '#0ff';
+  	  }
+  	  if ((x+y) == this.rover.pc + 1) {
+  	    color = '#0cc';
+  	  }
+  	  display.setOptions({fg: color});
   	  ostring = ostring.substr(ostring.length - 2);
-  	  display.drawText(this.x + x*3 + 1, this.y + y/0x10 + 4, ostring);
+  	  display.drawText(this.x + x*3 + 1, this.y + y/0x10 + 5, ostring);
+  	  display.setOptions({fg: oldcolor});
   	}
 	}
 }
