b56bf50f5362b3f2f651ae4dcfd1cccec9774cda
Paul Sanford <me@paulsanford.net>
2016-05-23 14:49:20 -0400

    Added visual heading display

--- roverstatus.js
+++ roverstatus.js
@@ -28,4 +28,16 @@ RoverStatus.prototype.draw = function(display) {
   	  display.setOptions({fg: oldcolor});
   	}
 	}
+
+  display.draw(this.x + 12, this.y + 1, String.fromCharCode(0x250C), this.rover.get_heading() === 7 ? "#f00" : undefined);
+  display.draw(this.x + 13, this.y + 1, "^", this.rover.get_heading() === 0 ? "#f00" : undefined);
+  display.draw(this.x + 14, this.y + 1, String.fromCharCode(0x2510), this.rover.get_heading() === 1 ? "#f00" : undefined);
+
+  display.draw(this.x + 12, this.y + 2, "<", this.rover.get_heading() === 6 ? "#f00" : undefined);
+  display.draw(this.x + 13, this.y + 2, "R", "#ff0");
+  display.draw(this.x + 14, this.y + 2, ">", this.rover.get_heading() === 2 ? "#f00" : undefined);
+
+  display.draw(this.x + 12, this.y + 3, String.fromCharCode(0x2514), this.rover.get_heading() === 5 ? "#f00" : undefined);
+  display.draw(this.x + 13, this.y + 3, "v", this.rover.get_heading() === 4 ? "#f00" : undefined);
+  display.draw(this.x + 14, this.y + 3, String.fromCharCode(0x2519), this.rover.get_heading() === 3 ? "#f00" : undefined);
 }
